f344452e77292dac19d2fefc104cde8b47a712c3
Aaron Plasek <alp445@nyu.edu>
2014-10-05 02:22:51 -0400

    collect author, epigraph, epi attribution, location, publisher, and year. outputs to a csv that can be opened in excel

--- TEST-Database/epigraphscraper-working.py
+++ TEST-Database/epigraphscraper-working.py
@@ -21,12 +21,19 @@ for document in range(0, len(allFilesInDirectory)):                   #for loop 
     root, ext = os.path.splitext(allFilesInDirectory[document])        #select file extension for particular file "x" in the list "allFilesInDirectory"
     if (ext == '.xml'):                                         #if file ends in ".xml", read file 
     # open file to be read
-        print("TEXT " + str(document+1) + ': ' + allFilesInDirectory[document])
+        print("TEXT " + str(document+1) + ': ' + allFilesInDirectory[document])  #error check line; uncomment to see progress in terminal
         readfile = open(str(allFilesInDirectory[document]))	        #specify file "x" to be read & open file
         soup = BeautifulSoup(readfile)                           #make "soup" object of file to search 
     # collect author & epigraphs from individual file
         author_list = [author.text for author in soup('author')]          #collect entries tagged "author" and place it in the list "authorlist"
-        print(author_list)
+        title_list = [title.text for title in soup('title')]              #collect "title" tag entries
+        publication_date = [date.text for date in soup('date')]
+        publication_place = [pubplace.text for pubplace in soup('pubplace')]
+
+        if len(soup('publisher')) > 0:         
+            publishers = [publisher.text for publisher in soup('publisher')]
+        else: 
+            publishers = ['Unknown Publisher', 'Unknown Publisher','Unknown Publisher']
 
         if len(soup('epigraph')) > 0:
             epigraph_list = [epigraph.text for epigraph in soup('epigraph')]  #collect entries tagged "epigraph" and place it in the list "epigraphlist'; note that soup.find_all("tag") == soup("tag")
@@ -36,6 +43,7 @@ for document in range(0, len(allFilesInDirectory)):                   #for loop 
         else: 
             epigraph_list = ['No Epigraphs']
             epigraph_attribution = ['No Epigraphs']
+    
     # clean out "/n" characters in attribution
         for attribution in range(0,len(epigraph_attribution)):
             cleaned_text = ""
@@ -43,47 +51,85 @@ for document in range(0, len(allFilesInDirectory)):                   #for loop 
                 if character not in characters_to_be_removed_from_attribution:
                     cleaned_text += character 
             epigraph_attribution[attribution] = cleaned_text   
+    # clean out "\n" characters in author name
+        for authorname in range(0, len(author_list)):
+            cleaned_text = ""
+            for character in author_list[authorname]:
+                if character not in characters_to_be_removed_from_attribution:
+                    cleaned_text += character
+            author_list[authorname] = cleaned_text
+     # clean out "\n" characters in titles
+        for title in range(0, len(title_list)):
+            cleaned_text = ""
+            for character in title_list[title]:
+                if character not in characters_to_be_removed_from_attribution:
+                    cleaned_text += character
+            title_list[title] = cleaned_text
+        for place in range(0, len(publication_place)):
+            cleaned_text = ""
+            for character in publication_place[place]:
+                if character not in characters_to_be_removed_from_attribution:
+                    cleaned_text += character
+            publication_place[place] = cleaned_text
+    # clean publisher names
+        for pubs in range(0, len(publishers)):
+            cleaned_text = ""
+            for character in publishers[pubs]:
+                if character not in characters_to_be_removed_from_attribution:
+                    cleaned_text += character
+            publishers[pubs] = cleaned_text
+    # clean publication years        
+        for year in range(0, len(publication_date)):
+            cleaned_text = ""
+            for character in publication_date[year]:
+                if character not in characters_to_be_removed_from_attribution:
+                    cleaned_text += character
+            publication_date[year] = cleaned_text                                       
 
+        readfile.close() #close file "x"
 # Error Checking Print-To-Terminal: print all information collected
-        readfile.close()                                                 #close file "x"
         if (len(soup('epigraph')) == 0):                         #check if file has epigraphs                
-            print(allFilesInDirectory[document] + ": No epigraphs found." + '\n')       #Error Test
+#            print(allFilesInDirectory[document] + ": No epigraphs found." + '\n')       #Error Test
             epigraphlessFileCount += 1                                    #note file did not have epigraph
         else:
             for i in range(0, len(soup.findAll('epigraph'))):          
                 if (len(soup.findAll('author')) == 0):
-                    print("Unknown Author" + "\n" + allFilesInDirectory[document] + "\n" + "Text " + str(document+1) \
-                         + ", epigraph " + str(i+1) + "\n" + str(epigraph_attribution[i]) + "\n" + str(epigraph_list[i]) + "\n") 
+#                    print("Unknown Author" + "\n" +title_list[i]+ "\n" +allFilesInDirectory[document] + "\n" + "Text " + str(document+1) \
+#                         + ", epigraph " + str(i+1) + "\n" + str(epigraph_attribution[i]) + "\n" + str(epigraph_list[i]) + "\n") 
                     totalEpigraphCount += 1
                 else:    
-                    print(author_list[0] + "\n" + allFilesInDirectory[document] + "\n" + "Text " + str(document+1) \
-                         + ", epigraph " + str(i+1) + "\n" + str(epigraph_attribution[i]) + "\n" + str(epigraph_list[i]) + "\n") 
+#                    print(author_list[0] + "\n" + title_list[i]+ "\n" +allFilesInDirectory[document] + "\n" + "Text " + str(document+1) \
+#                         + ", epigraph " + str(i+1) + "\n" + str(epigraph_attribution[i]) + "\n" + str(epigraph_list[i]) + "\n") 
                     totalEpigraphCount += 1
+
+#output to a CSV file -- NOTE: need to wrap strings in a list for csvwriter to output properly
+       
+        with open('epigraph_metadata.csv', 'a') as csvfile: #output metadata
+            epi_meta = csv.writer(csvfile, dialect='excel')
+            #epi_meta.writerow('epigraph ID' + '  |  ' + 'File Name' + '  |  ' +'File ID' + '  |  ' +'Hypotext Author' + '  |  ' + '  |  '+ 'Title'+ 'Epigraph Attribution') 
+            for i in range(0,len(soup('epigraph'))):
+                if (len(soup('author')) ==0):
+                    epi_meta.writerow([str(i) + '|' + allFilesInDirectory[document] + '|'+ str(document) + '|' +  'Unknown Author' + '|' + str(title_list[0])+ '|' + str(epigraph_attribution[i]) + '|' + str(publishers[1])+ '|' + str(publication_place[1]) + '|' + str(publication_date[1])])           
+                else:
+                    epi_meta.writerow([str(i) + '|' + allFilesInDirectory[document] + '|'+ str(document) + '|' +  author_list[0] + '|' + str(title_list[0])+ '|' + str(epigraph_attribution[i])+ '|' +  str(publishers[1])+ '|' + str(publication_place[1]) + '|' + str(publication_date[1])])
+
+        with open('epigraph_list.csv', 'a') as csvfile: #output metadata
+            epi_list = csv.writer(csvfile, dialect='excel')
+            for i in range(0,len(soup('epigraph'))):
+                epi_list.writerow(['Text ' + str(document+1)+ ', epigraph ' + str(i+1)]) 
+                epi_list.writerow([epigraph_list[i]])           
+
+
         
 #Error Checking Print-To-Terminal: Print total number of epigraphs collected  
 print("TOTAl NUMBER OF EPIGRAPHS: " + str(totalEpigraphCount))
 print("TOTAL NUMBER OF FILES: " + str(len(allFilesInDirectory)))
 print("FILES WITHOUT EPIGRAPHS: " + str(epigraphlessFileCount))
 
-
-#brown.xml has 15 bibl entries and 25 epigraphs total
-
-
 #CODE SNIPPETS & USEFUL NOTES ----
 #Can directly access individual epigraph as follows:
 #soup('author')[0].text
 #soup('author')[1].text
-#soup('epigraph')[0].text
-#soup('epigraph')[1].text
-#etc.
-
-#THIS SHOULD WORK FOR OUTPUT TO CSV ----
-#out.writerow(authorlist[2] + "    " + allFilesInDirectory[x] + "    " + str(i) + "   " + epigraph[i])
-#out = csv.writer(open("epigraph.csv","wb"), delimiter='\t',quoting=csv.QUOTE_MINIMAL) 
+#etc. 
 
 #NOTE FOR BEAUTIFUL SOUP: soup('epigraph') == soup.find_all('epigraph')
-
-#CORRECT VALUES FOR TEST DATABASE ----
-#TOTAl NUMBER OF EPIGRAPHS: 28
-#TOTAL NUMBER OF FILES: 13
-#FILES WITHOUT EPIGRAPHS: 6
